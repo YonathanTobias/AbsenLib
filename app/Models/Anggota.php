1f92319688e01319d56b5b700e7828110220cf32
Yonathan Tobias Buttok <48828679+YonathanTobias@users.noreply.github.com>
2026-09-01 11:21:53 +0700

    Format Nama Anggota Title Case

--- app/Models/Anggota.php
+++ app/Models/Anggota.php
@@ -4,12 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Anggota extends Model
 {
     use HasFactory;
 
     protected $fillable = ['nomor_induk', 'nama', 'peran'];
+
+    /**
+     * Otomatis seragamkan nama menjadi Title Case (Huruf besar di setiap awal kata).
+     * Contoh: "yonathan tobias buttok" atau "YONATHAN TOBIAS" -> "Yonathan Tobias Buttok"
+     */
+    protected function nama(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $value ? ucwords(strtolower($value)) : $value,
+            set: fn ($value) => $value ? ucwords(strtolower(preg_replace('/\s+/', ' ', trim($value)))) : $value,
+        );
+    }
 
     public function absensis()
     {
